4b7f7177688b35426e22c6fbf67be313c5eb1e8b
alexeyYasinskiy <alexey_yasinskiy@list.ru>
2020-11-13 20:21:45 +0300

    added section slider

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ let gulp = require('gulp'),
 
 gulp.task('sass', function(){
       return  gulp.src('app/scss/**/*.scss')
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename({suffix : '.min'}))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 8 versions']
@@ -31,7 +31,9 @@ gulp.task('js', function(){
 gulp.task('script', function(){
     return gulp.src([
 'node_modules/slick-carousel/slick/slick.js',
-'node_modules/magnific-popup/dist/jquery.magnific-popup.js'])
+'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+'node_modules/mixitup/dist/mixitup.js',
+'app/js/jquery.rateyo.js'])
 .pipe(concat('libs.min.js'))
 .pipe(uglify())
 .pipe(gulp.dest('app/js'))
@@ -41,7 +43,8 @@ gulp.task('style', function(){
     return gulp.src([
 'node_modules/normalize.css/normalize.css',
 'node_modules/slick-carousel/slick/slick.css',
-'node_modules/magnific-popup/dist/magnific-popup.css'])
+'node_modules/magnific-popup/dist/magnific-popup.css',
+'app/css/jquery.rateyo.css'])
 .pipe(concat('libs.min.css'))
 .pipe(cssmin())
 .pipe(gulp.dest('app/css'))
